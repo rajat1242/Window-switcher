95e1ecf3330d43e058bdd0ba6bfc7fc6b634458b
Rasjat Syal <syalrajat7@gmail.com>
2022-03-29 11:45:17 -0700

    Window Switcher

---
+++ Window-Switcher/LinkedList.js
@@ -0,0 +1,52 @@
+//Creating Linked List
+
+function Linkedlist() {
+  //Creating a node function
+  function Node(content) {
+    this.content = content;
+    this.next = this;
+    this.prev = this;
+  }
+
+  this.head = null;
+  this.tail = null;
+
+  this.add = function (content) {
+    //if current object head is not there then create a new node
+    if (!this.head) {
+      this.head = this.tail = new Node(content);
+    } else {
+      this.tail.next = new Node(content);
+      this.tail.next.prev = this.tail;
+      this.tail = this.tail.next;
+      this.tail.next = this.head;
+      this.head.prev = this.tail;
+    }
+  };
+
+  this.move_to_front = function (node) {
+    if (node === this.head) {
+      return;
+    }
+
+    let temp = this.head;
+    let isTailNode = false;
+
+    while (temp.next !== node) temp = temp.next;
+
+    if (temp.next === this.tail) isTailNode = true;
+
+    if (!isTailNode) {
+      temp.next = node.next;
+      node.next.prev = temp;
+    } else {
+      this.tail = this.tail.prev;
+    }
+
+    node.next = this.head;
+    this.head.prev = node;
+    this.head = node;
+    this.tail.next = this.head;
+    this.head.prev = this.tail;
+  };
+}
---
+++ Window-Switcher/README.md
@@ -0,0 +1,5 @@
+# Window-Switcher  using Linked List
+Just hold ctrl key and press b or f to change between windows
+Just go to this link to see live-> https://shubham912.github.io/Window-Switcher/
+This is how you can build a simple window switching UI using your #linkedlist knowledge.
+Linked lists are the building blocks of modern #programming and are used widely for different purposes. 
